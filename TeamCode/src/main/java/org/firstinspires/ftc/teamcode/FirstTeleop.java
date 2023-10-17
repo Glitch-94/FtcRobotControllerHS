84a68b2afe8779b436f35c63989a7ec40e7cc1da
Harrison Shipp <shipph@gmail.com>
2023-10-17 17:55:24 -0600

    Finished Episode 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTeleop.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class FirstTeleop extends OpMode {
+    @Override
+    public void init() {
+        //happens once on init
+    }
+
+    @Override
+    public void init_loop() {
+        //happens repeatedly during init
+    }
+
+    @Override
+    public void start() {
+        //happens once after program starts
+    }
+
+    @Override
+    public void loop() {
+        //happens repeatedly during the program
+    }
+
+    @Override
+    public void stop() {
+        //happens once after stop
+    }
+}
